b7186400ba5195d937d41f6a6b0f72fb324d5fbb
Vladimir Malits <malits@Vladimirs-MacBook-Pro.local>
2025-10-27 15:21:22 +0200

    Add API rate limiting middleware to enhance request handling

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,11 @@ use App\Http\Controllers\Auth\V1\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
-    Route::post('login', [LoginController::class, '__invoke']);
-    Route::post('register', [RegisterController::class, '__invoke']);
-    Route::group(['middleware' => ['auth:sanctum']], base_path('routes/api/users.php'));
+Route::middleware(['throttle:limit'])->group(static function (): void {
+    Route::prefix('v1')->group(function () {
+        Route::post('login', [LoginController::class, '__invoke']);
+        Route::post('register', [RegisterController::class, '__invoke']);
+        Route::group(['middleware' => ['auth:sanctum']], base_path('routes/api/users.php'));
+    });
 });
 
